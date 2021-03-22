2008ce797d11d65a0a74092e151d182925fb174e
EduardoFernandesUA <edu.fernandes@ua.pt>
2021-03-22 17:57:37 +0000

    corrected Ex04

--- aula02/Ex04/Ex04.py
+++ aula02/Ex04/Ex04.py
@@ -26,4 +26,4 @@ os.write(1, cryptogram )
 
 decipher = ARC4.new(chave)
 decrypted = decipher.decrypt( cryptogram )
-print(decrypted.decode('utf-8'))
+# print(decrypted.decode('utf-8')) este print estava a entrar para o ficheiro o que estragava o seu resultado
